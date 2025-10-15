85b6411e57c95b9584cc893b9635416989f9f4be
BhavnoorKaur29 <bhavnoorkaur05@gmail.com>
2025-10-15 11:29:36 +0530

    modifications related to DB

--- db.js
+++ db.js
@@ -3,8 +3,8 @@ const { Pool } = require("pg");
 const pool = new Pool({
   user: "postgres",
   host: "localhost",
-  database: "mcq_system",
-  password: "root123",
+  database: "Path2Tech",
+  password: "Bhavnoor@123",
   port: 5432,
 });
 
