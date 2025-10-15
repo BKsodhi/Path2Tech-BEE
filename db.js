f13acf8cd08ed48c9e47e6ef72f2b8dd41660498
BKsodhi <sodhibhavnoor29@gmail.com>
2025-10-15 12:02:44 +0530

    modifications related to DB

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const { Pool } = require("pg");
 const pool = new Pool({
   user: "postgres",
   host: "localhost",
-  database: "Path2Tech",
+  database: "mcq_system",
   password: "Bhavnoor@123",
   port: 5432,
 });
